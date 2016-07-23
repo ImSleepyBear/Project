09da29044814ba2d7b24859606d28cebdcb969d5
moha0047 <imdifferent.hassler@gmail.com>
2016-07-23 16:33:18 +0200

    changes in login scene new scene added possible to change scene via buttons possible to exit application only from login scene

---
+++ src/projectcardgame/ChangeScene.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectcardgame;
+
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.MenuButton;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Mohini
+ */
+public class ChangeScene {
+    
+    public String login = "LogIn";
+    public String signup = "SignUp";
+    
+    public void change(ActionEvent event, String destination) {        
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("FXML" + destination + ".fxml"));
+            Scene scene = new Scene(root);
+            Stage appStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            appStage.setScene(scene);
+            appStage.show();
+            System.out.println("Destination: " + destination);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }  
+    
+//    public void change(ActionEvent event, String destination) {
+//
+//        try {
+//            Parent root = FXMLLoader.load(getClass().getResource("FXML" + destination + ".fxml"));
+//            Scene scene = new Scene(root);
+//            Stage appStage = (Stage) ((MenuButton) event.getSource()).getScene().getWindow();
+//            appStage.setScene(scene);
+//            appStage.show();
+//
+//        } catch (IOException ex) {
+//            ex.printStackTrace();
+//        }
+//    }  
+}
--- src/projectcardgame/FXMLLogin.java
+++ src/projectcardgame/FXMLLogin.java
@@ -10,6 +10,7 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
@@ -21,14 +22,36 @@ public class FXMLLogin implements Initializable {
     
     @FXML
     private TextField txtField_username;
-
     @FXML
     private TextField txtField_password;
+    @FXML
+    private Button login;
+    @FXML
+    private Button exit;
+    @FXML
+    private Button sign_up;
+    @FXML
+    private Button recover_credentials;
     
-    
+    private ChangeScene cs = new ChangeScene();
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
+    private void login(ActionEvent event) {
+        
+    }
+    
+    @FXML
+    private void exit(ActionEvent event) {
+        System.exit(0);
+    }
+    
+    @FXML
+    private void signUp(ActionEvent event) {
+        cs.change(event, cs.signup);
+    }
+    
+    @FXML
+    private void recoverCredentials(ActionEvent event) {
         
     }
     
