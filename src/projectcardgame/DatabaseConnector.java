fa48fc9d485bc62c7b616df827d105401fd812c0
mohini <mohini@MohinisLaptop>
2016-11-04 14:01:24 +0100

    added variables and methods in database connector

--- src/projectcardgame/DatabaseConnector.java
+++ src/projectcardgame/DatabaseConnector.java
@@ -5,10 +5,28 @@
  */
 package projectcardgame;
 
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
+import com.mysql.jdbc.Statement;
+import java.sql.ResultSet;
+
 /**
  *
  * @author mohini
  */
 public class DatabaseConnector {
     
+    String url = null;
+    String user = null;
+    String password = null;
+    
+    Connection connection = null;
+    Statement statement = null;
+    ResultSet resultSet = null;
+    
+    PreparedStatement preparedStatement = null;
+    
+    public void connect(){
+        
+    }
 }
