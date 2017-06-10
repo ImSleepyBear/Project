2031e0feff0574238dc381187ea8a4431e021732
moha0047 <imdifferent.hassler@gmail.com>
2017-06-10 12:34:30 +0200

    small cleanup of multiline comments in controllers

--- src/controllers/FXMLExploreCardController.java
+++ src/controllers/FXMLExploreCardController.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package controllers;
 
 import java.net.URL;
@@ -16,9 +12,6 @@ import javafx.fxml.Initializable;
  */
 public class FXMLExploreCardController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
